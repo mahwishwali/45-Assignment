2f796b3cc6a8f62658f64f8d7e32e06a1c610661
mahwishwali <160737705+mahwishwali@users.noreply.github.com>
2024-07-11 02:30:32 +0500

    Add files via upload

---
+++ Q15-changing Guest list/changing.ts
@@ -0,0 +1,34 @@
+
+//Q15- Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations.
+// You’ll have to think of someone else to invite.
+
+//• Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
+
+//• Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+
+//• Print a second set of invitation messages, one for each person who is still in your list.
+
+
+let Guest_list :string[] = ['imran khan', 'wasim akram', 'shahid afridi'];
+
+for(let i=0; i<Guest_list.length; i++){
+
+   console.log('Dear Mr. ' + Guest_list[i] + ', \n\nIt is our  pleasure to invite you in our party . \n\n thankyou!\n\n')
+}
+
+
+let absent_Guest: string = 'Imran khan';
+
+let new_Guest: string = 'kamran Tessori';
+
+ Guest_list[0] = new_Guest;
+
+for(let i=0; i<Guest_list.length; i++){
+
+   console.log('Dear Mr. ' + Guest_list[i] + ', \n\nIt is pleasure to invite you in our party . \n\n thankyou!\n\n')
+}
+
+
+    console.log (`Mr.${absent_Guest} is not coming to party.`)
+
+    
