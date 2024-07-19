92da2f293507095d666a1629780e0722772312e5
mahwishwali <160737705+mahwishwali@users.noreply.github.com>
2024-07-20 02:46:35 +0500

    Add files via upload

---
+++ Q20- Think of something/Empty_Array.js
@@ -0,0 +1,11 @@
+"use strict";
+//Q20- Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages,
+// or anything else you’d like.
+// Write a program that creates a list containing these items.
+let item = ['pakistan', 'karachi', 'urdu'];
+//write a program that crestes a list containting these item.
+let item2 = [];
+item2.push('germany');
+item2.push('England');
+item2.push('India');
+console.log(item2);
---
+++ Q20- Think of something/Empty_Array.ts
@@ -0,0 +1,15 @@
+
+//Q20- Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages,
+// or anything else you’d like.
+// Write a program that creates a list containing these items.
+
+
+let item:string[] = ['pakistan','karachi','urdu'];
+//write a program that crestes a list containting these item.
+
+let item2 :string[] = [] ;
+item2.push('germany');
+item2.push('England');
+item2.push('India');
+console.log(item2);
+
